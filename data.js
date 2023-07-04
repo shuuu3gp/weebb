b6702d13745f1544b52f6fc2c4aefb4acf6cd544
shuuu3gp <138618274+shuuu3gp@users.noreply.github.com>
2023-07-05 01:43:10 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+
+// click di luar side nbar
+const menu = document.querySelector('#menu')
+
+document.addEventListener('click', function (e) {
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
+
+
+
+
+
